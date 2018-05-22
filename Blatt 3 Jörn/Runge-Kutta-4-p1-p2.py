1a6872decce1372069f4e5cf139b2b1139e4230f
Fennastege <38552633+Fennastege@users.noreply.github.com>
2018-05-22 12:34:44 +0200

    Update Runge-Kutta-4-p1-p2.py
    In der rk4 nicht x+0.5*h*k1 sondern x+0.5*k1 bitte noch einmal theorie dazu lesen.
    gamma1=0.02
    und die Übergabe von funkitonen in funktionen ist schwierig. Python erkennt f nicht als funktion wenn du nicht die entsprechenden parameter mit angibst die übergeben werden müssen

--- Blatt 3 Jörn/Runge-Kutta-4-p1-p2.py
+++ Blatt 3 Jörn/Runge-Kutta-4-p1-p2.py
@@ -14,21 +14,21 @@ def f(population,t):
     p1,p2=population
     epsilon1 = 2.
     epsilon2 = 0.8
-    gamma1 = 0.2
+    gamma1 = 0.02
     gamma2 = 0.0002
     return np.array([ p1*(epsilon1-gamma1*p2) , -p2*(epsilon2 - gamma2*p1) ]) 
 
 #Definiere Runge-Kuta 4 Verfahren 
-def rk4(f,Tende,p_0):
+def rk4(Tende,p_0):
     x = p_0 
     h=0.025 
     t=0
     plt.plot(x[0],x[1],'b.')
     while t<=Tende:              
         k1 = f( x, t )
-        k2 = f( x + 0.5 * h * k1, t + 0.5 * h ) 
-        k3 = f( x + 0.5 * h * k2, t + 0.5 * h )  
-        k4 = f( x + h * k3, t + h  )
+        k2 = f( x + 0.5 *  k1, t + 0.5 * h ) 
+        k3 = f( x + 0.5 *  k2, t + 0.5 * h )  
+        k4 = f( x + k3, t + h  )
         x = x + h * ( ( k1 + 2.0 * ( k2 + k3 ) + k4 ) / 6.0 )
         t=t+h
         plt.plot(x[0],x[1],'b.')
@@ -43,6 +43,6 @@ T=20
 #Schrittweite
 h=0.025
 plt.xlabel('Population Beute')
-rk4(f,20,p_0)
+rk4(20,p_0)
 plt.ylabel('Population Räuber')
 plt.show()
