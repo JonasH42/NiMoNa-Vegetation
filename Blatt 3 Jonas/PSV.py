35d92ed638f08b451fd7a7a0c350b3df9eb0d4cc
JonasH42 <jonas.hallekamp@wwu.de>
2018-05-23 17:41:49 +0200

    Nach 7 Iteration läuft die Funktion über,trotz Dealiasing. Bisher konnte ich den Fehler nicht finden

---
+++ Blatt 3 Jonas/PSV.py
@@ -0,0 +1,60 @@
+import numpy as np
+import matplotlib.pyplot as plt
+#import time
+
+#Anfangsbedingungen
+gamma_sigma = 1.
+mu = 0.01
+h = 0.005
+L = np.array([4 * np.pi, 4 * np.pi])
+N = np.array([256,256])
+Tend = 10
+
+
+def rk4(n,k,h):
+    k1 = f(n,k)
+    k2 = f(n+h/2*k1,k)
+    k3 = f(n+h/2*k2,k)
+    k4 = f(n+h*k3,k)
+    return n + h/6*(k1+2.*k2+2.*k3+k4)
+
+def f(n,k):
+    return n * (gamma_sigma - mu - k**2) - np.fft.fft(np.fft.ifft(dealiasing(n,0.01))**2)
+
+def n_init(x,L):
+    return 0.2*np.exp( ((x[0]-L[0]/2.0)**2 + (x[1]-L[1]/2.0)**2)/-0.5)
+
+def dealiasing(n,D):
+    kmax = max(k)
+    for i in range(len(n)):
+        if k[i] < kmax * D:
+            n[i] = 0
+    return n
+
+
+N_t = int(Tend/h)
+    
+k = np.fft.fftfreq(N[0],L[0]/(N[0]*2*np.pi))
+T = np.arange(N_t)*h
+    
+x = np.arange(N[0])*L[0]/N[0]
+y = np.zeros(N[1]) + L[1]/2.
+n = np.zeros((N_t,N[0]))
+n[0] = n_init([x,y],L)
+    
+for i in range(1,N_t):
+    nk = np.fft.fft(n[i-1])
+    nk_new = rk4(nk,k,h)
+    n[i] = np.fft.ifft(nk_new).real
+        
+
+plt.ion()
+plt.show()
+for i in range(8):
+    #if i%10 == 0:
+        plt.cla()
+        plt.plot(x,n[i])
+        plt.draw()
+        plt.pause(0.1)
+        
+plt.ioff
