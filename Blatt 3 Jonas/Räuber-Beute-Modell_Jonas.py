f149c7d872286d8fb76c3b9e8b5f8042d43734f9
Fennastege <38552633+Fennastege@users.noreply.github.com>
2018-05-25 12:35:56 +0200

    Update Räuber-Beute-Modell_Jonas.py
    mit Diffusionskonstante D_n

--- Blatt 3 Jonas/Räuber-Beute-Modell_Jonas.py
+++ Blatt 3 Jonas/Räuber-Beute-Modell_Jonas.py
@@ -1,72 +1,60 @@
 import numpy as np
 import matplotlib.pyplot as plt
+#import time
 
-class RBM:
+#Anfangsbedingungen
+gamma_sigma = 1.
+mu = 0.01
+h = 0.005
+L = np.array([4 * np.pi, 4 * np.pi])
+N = np.array([256,256])
+Tend = 100
+D_n=0.01
+
+def rk4(n,k,h):
+    k1 = f(n,k)
+    k2 = f(n+h/2*k1,k)
+    k3 = f(n+h/2*k2,k)
+    k4 = f(n+h*k3,k)
+    return n + h/6*(k1+2.*k2+2.*k3+k4)
+
+def f(n,k):
+    return -D_n*k**2*n+(gamma_sigma - mu)*n- np.fft.fft(np.fft.ifft(dealiasing(n))**2)
+
+def n_init(x,L):
+    return 0.2*np.exp( ((x[0]-L[0]/2.0)**2 + (x[1]-L[1]/2.0)**2)/-0.5)
+
+def dealiasing(n):
+    kmax = max(k)
+    for i in range(len(n)):
+        if k[i] > 2./3.*kmax :
+            n[i] = 0
+    return n
+
+
+N_t = int(Tend/h)
     
-    def __init__(self):
-        NaN = np.NaN
-        self.h = 0.025
-        self.N_t = 30000
-        self.eps1 = 2.
-        self.eps2 = 0.8
-        self.gamma1 = 0.02
-        self.gamma2 = 0.0002 
-        self.s = 4
-        self.a = [0.0,0.5,0.5,1.0]
-        self.b = [1/6.,1/3.,1/3.,1/6.]
-        self.c  = [[NaN,NaN,NaN,NaN],
-                   [1/2.,NaN,NaN,NaN],
-                   [0.0,1/2.,NaN,NaN], 
-                   [0.0,0.0,1.,NaN]]
-
-
-
-    def P1(self,p1,p2):
-        return(p1*(self.eps1-self.gamma1*p2))
-
-    def P2(self,p2,p1):
-        return(-p2*(self.eps2-self.gamma2*p1))
-
-    def kj(self,f,x,x2,j,k):
-        summe = 0.
-        for i in range(0,j):
-            summe = summe + self.h*self.c[j][i]*k[i] 
+k = np.fft.fftfreq(N[0],L[0]/(N[0]*2*np.pi))
+T = np.arange(N_t)*h
     
-        return f(x + summe,x2 + summe) 
+x = np.arange(N[0])*L[0]/N[0]
+y = np.zeros(N[1]) + L[1]/2.
+n = np.zeros((N_t,N[0]))
+n[0] = n_init([x,y],L)
     
-    
-    def run(self):
-        p1 = [200.]
-        p2 = [20.]
-        t = [0.]
-        for i in range(0,self.N_t):
-            k1 = []
-            k2 = []
-            t.append(t[len(t)-1]+self.h)
-            for j in range(0,self.s):
-                k1.append(self.kj(self.P1,p1[i],p2[i],j,k1))
-                k2.append(self.kj(self.P2,p2[i],p1[i],j,k2))
-            
-            p1_temp = p1[i]
-            p2_temp = p2[i]
-            for m in range(0,len(k1)):
-                p1_temp = p1_temp + self.h*self.b[m]*k1[m]
-                p2_temp = p2_temp + self.h*self.b[m]*k2[m]
-            
-            p1.append(p1_temp)
-            p2.append(p2_temp)
-        return p1,p2,t
-#Schrittweite
+for i in range(1,N_t):
+    nk = np.fft.fft(n[i-1])
+    nk_new = rk4(nk,k,h)
+    n[i] = np.fft.ifft(nk_new).real
+        
 
-
-
-
-rbm = RBM()
-f = plt.figure()
-p1,p2,t = rbm.run()
-plt.plot(p1,p2)
-#plt.plot(t,p1)
-#plt.plot(t,p2)
-
+plt.ion()
 plt.show()
-f.savefig('p1gegenp2.png')
+for i in range(100):
+    #if i%10 == 0:
+        plt.cla()
+        plt.plot(x,n[i])
+        plt.draw()
+        plt.pause(0.1)
+        
+plt.ioff
